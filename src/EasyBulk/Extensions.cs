f63f0151f892b947e22872007bf2c9fbc619de76
michele <mikele.mercadante@gmail.com>
2024-04-20 20:29:13 +0200

    chore: refactoring

--- src/EasyBulk/Extensions.cs
+++ src/EasyBulk/Extensions.cs
@@ -5,33 +5,61 @@ namespace EasyBulk;
 
 public static class Extensions
 {
-    public static async Task Bulk<T>(this SqlConnection connection, string destinationTableName, IEnumerable<T> data, IEnumerable<IColumnDataMapper<T>> mappings)
+    public static Task Bulk<T>(this SqlConnection connection, string destinationTableName, IEnumerable<T> data, IEnumerable<IColumnDataMapper<T>> mappings)
     {
-        using var table = new DataTable(destinationTableName);
-        foreach(var mapping in mappings)
-            table.Columns.Add(mapping.ColumnName, mapping.ColumnType);
+        var bulk = new BulkOperation<T>(destinationTableName);
 
-        var rows = data.Select(d => 
+        bulk.CreateDataTable(mappings);
+        bulk.FillDataTable(data);
+        return bulk.Execute(connection);
+    }
+}
+
+internal class BulkOperation<T>
+{
+    private readonly string _tableName;
+    private IEnumerable<IColumnDataMapper<T>> _mappings;
+    private DataTable _table;
+
+    public BulkOperation(string destinationTable)
+    {
+        _tableName = destinationTable;
+    }
+
+    public Task Execute(SqlConnection connection)
+    {
+        using var bulk = new SqlBulkCopy(connection, SqlBulkCopyOptions.Default, null);
+
+        bulk.DestinationTableName = _tableName;
+        foreach (DataColumn column in _table.Columns)
+            bulk.ColumnMappings.Add(column.ColumnName, column.ColumnName);
+
+        return bulk.WriteToServerAsync(_table);
+    }
+
+    public void FillDataTable(IEnumerable<T> data)
+    {
+        var rows = data.Select(d =>
         {
-            var row = table.NewRow();
-            
-            foreach(var mapping in mappings)
+            var row = _table.NewRow();
+
+            foreach (var mapping in _mappings)
                 row[mapping.ColumnName] = mapping.DataSelector(d) ?? DBNull.Value;
 
             return row;
         });
 
-        foreach(var row in rows)
-            table.Rows.Add(row);
-        
-        table.AcceptChanges();
+        foreach (var row in rows)
+            _table.Rows.Add(row);
 
-        using var bulk = new SqlBulkCopy(connection, SqlBulkCopyOptions.Default, null);
+        _table.AcceptChanges();
+    }
 
-        bulk.DestinationTableName = destinationTableName;
-        foreach(DataColumn column in table.Columns)
-            bulk.ColumnMappings.Add(column.ColumnName, column.ColumnName);
-        
-        await bulk.WriteToServerAsync(table);
+    public void CreateDataTable(IEnumerable<IColumnDataMapper<T>> mappings)
+    {
+        _mappings = mappings;
+        _table = new DataTable(_tableName);
+        foreach (var mapping in _mappings)
+            _table.Columns.Add(mapping.ColumnName, mapping.ColumnType);
     }
 }
